0afc5fc46012139d21ac6d1e27d8ba5746204af7
mcgear <michael.gearhardt@gmail.com>
2024-03-18 08:11:47 -0600

    ref updates

--- src/src.deps.ts
+++ src/src.deps.ts
@@ -1,5 +1,8 @@
-export { type ComponentChildren, type JSX } from "preact";
-export { useEffect, useState } from "preact/hooks";
+export {
+  type ComponentChildren,
+  type JSX,
+} from "https://esm.sh/preact@10.19.2";
+export { useEffect, useState } from "https://esm.sh/preact@10.19.2/hooks";
 export { render as renderMarkdown } from "https://deno.land/x/gfm@0.2.5/mod.ts";
 import npmMoment from "npm:moment";
 export const moment = npmMoment;
