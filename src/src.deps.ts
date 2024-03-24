765a31c289dc5d5779f48e069f33651b3389aa14
mcgear <michael.gearhardt@gmail.com>
2024-03-24 16:27:13 -0600

    ref updates

--- src/src.deps.ts
+++ src/src.deps.ts
@@ -1,12 +1,12 @@
 export {
   type ComponentChildren,
   type JSX,
-} from "https://esm.sh/preact@10.19.6";
-export { useEffect, useState } from "https://esm.sh/preact@10.19.6/hooks";
+} from "https://esm.sh/preact@10.20.1";
+export { useEffect, useState } from "https://esm.sh/preact@10.20.1/hooks";
 export { render as renderMarkdown } from "https://deno.land/x/gfm@0.2.5/mod.ts";
 import npmMoment from "npm:moment";
 export const moment = npmMoment;
 
 export * from "https://deno.land/x/fathym_common@v0.0.177/mod.ts";
 
-export * from "https://deno.land/x/fathym_atomic@v0.0.137/mod.ts";
+export * from "https://deno.land/x/fathym_atomic@v0.0.139/mod.ts";
